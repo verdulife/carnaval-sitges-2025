fa9a565697956738a5f7d49defb1806850833330
verdulife <verdu@live.com>
2025-01-14 00:53:46 +0100

    generate base

---
+++ src/metadata.ts
@@ -0,0 +1,7 @@
+export const site = {
+  name: "Carnaval Sitges 2025",
+  title: "Carnaval de Sitges 2025",
+  description: "Aplicació web per el Carnaval de Sitges 2025",
+  image: "/card.jpg",
+  url: "https://www.victorbregante.com/carnaval-sitges-2025",
+}
