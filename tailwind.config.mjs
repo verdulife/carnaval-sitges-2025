4b183d8982781c63ceb8418ab9026fa5406de8d5
Gaplogic <estudio@gaplogic.com>
2025-01-14 14:15:04 +0100

    fix filter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"filter-red": "var(--filter-red)",
+				"filter-blue": "var(--filter-blue)",
+			},
+		},
 	},
 	plugins: [],
 }
