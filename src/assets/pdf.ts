f39b05c273a83afdfa941a80deaac320ca41bad6
verdulife <verdu@live.com>
2025-02-12 16:22:10 +0100

    change pdf to images

---
+++ src/assets/pdf.ts
@@ -0,0 +1,174 @@
+export const pages = [
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0001.jpg",
+    alt: "Programa Carnaval 2025 - Página 1",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0002.jpg",
+    alt: "Programa Carnaval 2025 - Página 2",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0003.jpg",
+    alt: "Programa Carnaval 2025 - Página 3",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0004.jpg",
+    alt: "Programa Carnaval 2025 - Página 4",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0005.jpg",
+    alt: "Programa Carnaval 2025 - Página 5",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0006.jpg",
+    alt: "Programa Carnaval 2025 - Página 6",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0007.jpg",
+    alt: "Programa Carnaval 2025 - Página 7",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0008.jpg",
+    alt: "Programa Carnaval 2025 - Página 8",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0009.jpg",
+    alt: "Programa Carnaval 2025 - Página 9",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0010.jpg",
+    alt: "Programa Carnaval 2025 - Página 10",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0011.jpg",
+    alt: "Programa Carnaval 2025 - Página 11",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0012.jpg",
+    alt: "Programa Carnaval 2025 - Página 12",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0013.jpg",
+    alt: "Programa Carnaval 2025 - Página 13",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0014.jpg",
+    alt: "Programa Carnaval 2025 - Página 14",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0015.jpg",
+    alt: "Programa Carnaval 2025 - Página 15",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0016.jpg",
+    alt: "Programa Carnaval 2025 - Página 16",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0017.jpg",
+    alt: "Programa Carnaval 2025 - Página 17",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0018.jpg",
+    alt: "Programa Carnaval 2025 - Página 18",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0019.jpg",
+    alt: "Programa Carnaval 2025 - Página 19",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0020.jpg",
+    alt: "Programa Carnaval 2025 - Página 20",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0021.jpg",
+    alt: "Programa Carnaval 2025 - Página 21",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0022.jpg",
+    alt: "Programa Carnaval 2025 - Página 22",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0023.jpg",
+    alt: "Programa Carnaval 2025 - Página 23",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0024.jpg",
+    alt: "Programa Carnaval 2025 - Página 24",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0025.jpg",
+    alt: "Programa Carnaval 2025 - Página 25",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0026.jpg",
+    alt: "Programa Carnaval 2025 - Página 26",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0027.jpg",
+    alt: "Programa Carnaval 2025 - Página 27",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0028.jpg",
+    alt: "Programa Carnaval 2025 - Página 28",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0029.jpg",
+    alt: "Programa Carnaval 2025 - Página 29",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0030.jpg",
+    alt: "Programa Carnaval 2025 - Página 30",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0031.jpg",
+    alt: "Programa Carnaval 2025 - Página 31",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0032.jpg",
+    alt: "Programa Carnaval 2025 - Página 32",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0033.jpg",
+    alt: "Programa Carnaval 2025 - Página 33",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0034.jpg",
+    alt: "Programa Carnaval 2025 - Página 34",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0035.jpg",
+    alt: "Programa Carnaval 2025 - Página 35",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0036.jpg",
+    alt: "Programa Carnaval 2025 - Página 36",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0037.jpg",
+    alt: "Programa Carnaval 2025 - Página 37",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0038.jpg",
+    alt: "Programa Carnaval 2025 - Página 38",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0039.jpg",
+    alt: "Programa Carnaval 2025 - Página 39",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0040.jpg",
+    alt: "Programa Carnaval 2025 - Página 40",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0041.jpg",
+    alt: "Programa Carnaval 2025 - Página 41",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0042.jpg",
+    alt: "Programa Carnaval 2025 - Página 42",
+  },
+  {
+    src: "/pdf/programa-carnaval-2025_pages-to-jpg-0043.jpg",
+    alt: "Programa Carnaval 2025 - Página 43",
+  }
+]
