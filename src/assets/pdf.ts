6ae7b62f83c10f7c5f27309d3d6ed48c0004d423
verdulife <verdu@live.com>
2025-02-21 20:17:32 +0100

    update programa ok

--- src/assets/pdf.ts
+++ src/assets/pdf.ts
@@ -1,174 +1,174 @@
 export const pages = [
   {
-    src: "/pdf/page-1.jpg",
+    src: "/pdf/1.jpg",
     alt: "Pàgina 1",
   },
   {
-    src: "/pdf/page-2.jpg",
+    src: "/pdf/2.jpg",
     alt: "Pàgina 2",
   },
   {
-    src: "/pdf/page-3.jpg",
+    src: "/pdf/3.jpg",
     alt: "Pàgina 3",
   },
   {
-    src: "/pdf/page-4.jpg",
+    src: "/pdf/4.jpg",
     alt: "Pàgina 4",
   },
   {
-    src: "/pdf/page-5.jpg",
+    src: "/pdf/5.jpg",
     alt: "Pàgina 5",
   },
   {
-    src: "/pdf/page-6.jpg",
+    src: "/pdf/6.jpg",
     alt: "Pàgina 6",
   },
   {
-    src: "/pdf/page-7.jpg",
+    src: "/pdf/7.jpg",
     alt: "Pàgina 7",
   },
   {
-    src: "/pdf/page-8.jpg",
+    src: "/pdf/8.jpg",
     alt: "Pàgina 8",
   },
   {
-    src: "/pdf/page-9.jpg",
+    src: "/pdf/9.jpg",
     alt: "Pàgina 9",
   },
   {
-    src: "/pdf/page-10.jpg",
+    src: "/pdf/10.jpg",
     alt: "Pàgina 10",
   },
   {
-    src: "/pdf/page-11.jpg",
+    src: "/pdf/11.jpg",
     alt: "Pàgina 11",
   },
   {
-    src: "/pdf/page-12.jpg",
+    src: "/pdf/12.jpg",
     alt: "Pàgina 12",
   },
   {
-    src: "/pdf/page-13.jpg",
+    src: "/pdf/13.jpg",
     alt: "Pàgina 13",
   },
   {
-    src: "/pdf/page-14.jpg",
+    src: "/pdf/14.jpg",
     alt: "Pàgina 14",
   },
   {
-    src: "/pdf/page-15.jpg",
+    src: "/pdf/15.jpg",
     alt: "Pàgina 15",
   },
   {
-    src: "/pdf/page-16.jpg",
+    src: "/pdf/16.jpg",
     alt: "Pàgina 16",
   },
   {
-    src: "/pdf/page-17.jpg",
+    src: "/pdf/17.jpg",
     alt: "Pàgina 17",
   },
   {
-    src: "/pdf/page-18.jpg",
+    src: "/pdf/18.jpg",
     alt: "Pàgina 18",
   },
   {
-    src: "/pdf/page-19.jpg",
+    src: "/pdf/19.jpg",
     alt: "Pàgina 19",
   },
   {
-    src: "/pdf/page-20.jpg",
+    src: "/pdf/20.jpg",
     alt: "Pàgina 20",
   },
   {
-    src: "/pdf/page-21.jpg",
+    src: "/pdf/21.jpg",
     alt: "Pàgina 21",
   },
   {
-    src: "/pdf/page-22.jpg",
+    src: "/pdf/22.jpg",
     alt: "Pàgina 22",
   },
   {
-    src: "/pdf/page-23.jpg",
+    src: "/pdf/23.jpg",
     alt: "Pàgina 23",
   },
   {
-    src: "/pdf/page-24.jpg",
+    src: "/pdf/24.jpg",
     alt: "Pàgina 24",
   },
   {
-    src: "/pdf/page-25.jpg",
+    src: "/pdf/25.jpg",
     alt: "Pàgina 25",
   },
   {
-    src: "/pdf/page-26.jpg",
+    src: "/pdf/26.jpg",
     alt: "Pàgina 26",
   },
   {
-    src: "/pdf/page-27.jpg",
+    src: "/pdf/27.jpg",
     alt: "Pàgina 27",
   },
   {
-    src: "/pdf/page-28.jpg",
+    src: "/pdf/28.jpg",
     alt: "Pàgina 28",
   },
   {
-    src: "/pdf/page-29.jpg",
+    src: "/pdf/29.jpg",
     alt: "Pàgina 29",
   },
   {
-    src: "/pdf/page-30.jpg",
+    src: "/pdf/30.jpg",
     alt: "Pàgina 30",
   },
   {
-    src: "/pdf/page-31.jpg",
+    src: "/pdf/31.jpg",
     alt: "Pàgina 31",
   },
   {
-    src: "/pdf/page-32.jpg",
+    src: "/pdf/32.jpg",
     alt: "Pàgina 32",
   },
   {
-    src: "/pdf/page-33.jpg",
+    src: "/pdf/33.jpg",
     alt: "Pàgina 33",
   },
   {
-    src: "/pdf/page-34.jpg",
+    src: "/pdf/34.jpg",
     alt: "Pàgina 34",
   },
   {
-    src: "/pdf/page-35.jpg",
+    src: "/pdf/35.jpg",
     alt: "Pàgina 35",
   },
   {
-    src: "/pdf/page-36.jpg",
+    src: "/pdf/36.jpg",
     alt: "Pàgina 36",
   },
   {
-    src: "/pdf/page-37.jpg",
+    src: "/pdf/37.jpg",
     alt: "Pàgina 37",
   },
   {
-    src: "/pdf/page-38.jpg",
+    src: "/pdf/38.jpg",
     alt: "Pàgina 38",
   },
   {
-    src: "/pdf/page-39.jpg",
+    src: "/pdf/39.jpg",
     alt: "Pàgina 39",
   },
   {
-    src: "/pdf/page-40.jpg",
+    src: "/pdf/40.jpg",
     alt: "Pàgina 40",
   },
   {
-    src: "/pdf/page-41.jpg",
+    src: "/pdf/41.jpg",
     alt: "Pàgina 41",
   },
   {
-    src: "/pdf/page-42.jpg",
+    src: "/pdf/42.jpg",
     alt: "Pàgina 42",
   },
   {
-    src: "/pdf/page-43.jpg",
+    src: "/pdf/43.jpg",
     alt: "Pàgina 43",
   }
 ]
